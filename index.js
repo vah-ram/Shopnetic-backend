7f872fb573ada4b5fe288eafe36402bafadfd4ed
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-06-06 22:46:39 +0400

    changed index

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server( server, {
     cors: {
-        origin: "http://shopnetic.wuaze.com",
+        origin: "https://shopnetic.wuaze.com",
         methods: ['GET', 'POST']
     }
 });
