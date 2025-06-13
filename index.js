b59b49991003cf711772d6bfb9a3c68b7b9416fb
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-06-13 17:17:58 +0400

    lol

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 import mongoose from 'mongoose';
 import cors from 'cors'
 import { Server } from 'socket.io';
-import http from 'http'
+import http from 'http';
+import dotenv from "dotenv";
 import SignRouter from './controllers/SignController.js'
 import ProductRouter from './controllers/ProductController.js'
 import TodoRouter from './controllers/TodoController.js'
@@ -11,6 +12,7 @@ import SearchRouter from './controllers/SearchController.js'
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 app.use('/api/auth', SignRouter);
 app.use('/api/product', ProductRouter);
 app.use('/api/todo', TodoRouter);
@@ -20,18 +22,19 @@ const server = http.createServer(app);
 
 const io = new Server( server, {
     cors: {
-        origin: "https://shopnetic.wuaze.com",
+        origin: process.env.FrontEnd_Port,
         methods: ['GET', 'POST']
     }
 });
 
-// io.on("connection", (socket) => {
-// });
+io.on("connection", (socket) => {
+    console.log(`Socket Connected Id: ${socket.id}`)
+});
 
-mongoose.connect('mongodb+srv://Vahram:vahram12345@cluster0.powdqsm.mongodb.net/Shopnetic')
+mongoose.connect(process.env.Mongo_Port)
     .then(() => console.log("✅ MongoDB Connected"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 
-server.listen(5000, () => {
+server.listen( process.env.Backend_Port, () => {
     console.log("Server is listening!")
 });
